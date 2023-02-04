a66a7169fe8e0753086efd3f5a4708a505f5c225
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-02-04 12:26:29 -0300

    Implementa ataque básico das plantas

--- GGJ_2023/Assets/Scripts/Plant/Plant.cs
+++ GGJ_2023/Assets/Scripts/Plant/Plant.cs
@@ -11,31 +11,57 @@ public class Plant : MonoBehaviour
     [SerializeField]
     private GameObject enemy;
     private bool onRest;
+    private GameObject currentTarget;
+    private List<GameObject> enemies;
 
     void Start()
     {
         //GetComponent<SpriteRenderer>().sprite = plantObject.sprite;
+        GetComponent<BoxCollider2D>().size = (2*plantObject.range + 1)*Vector2.one;
+        enemies = new List<GameObject>();
     }
 
     void Update()
     {
-        if (!onRest)
+        if (!onRest && enemies.Count!=0)
         {
             if(plantObject.plantType == PlantType.ShootingDamage)
                 StartCoroutine(Shoot());
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gnome"))
+        {
+            enemies.Add(collision.gameObject);
+            if (enemies.Count == 1)
+                currentTarget = enemies[0];
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gnome"))
+        {
+            enemies.Remove(collision.gameObject);
+            if (enemies.Count != 0)
+                currentTarget = enemies[0];
+            else
+                currentTarget = null;
+        } 
+    }
+
     private IEnumerator Shoot()
     {
-        enemy.GetComponent<EnemyDamage>().TakeDamage(plantObject.damage);
+        currentTarget.GetComponent<EnemyDamage>().TakeDamage(plantObject.damage);
 
         var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         onRest = true;
-        var direction = (enemy.transform.position - transform.position).normalized;
+        var direction = (currentTarget.transform.position - transform.position).normalized;
         bullet.GetComponent<Rigidbody2D>().velocity = direction * plantObject.bulletSpeed;
 
-        var time = (enemy.transform.position - transform.position).magnitude / (direction * plantObject.bulletSpeed).magnitude;
+        var time = (currentTarget.transform.position - transform.position).magnitude / (direction * plantObject.bulletSpeed).magnitude;
         var waitTime = plantObject.rechargeTime - time;
 
         if (waitTime > 0)
