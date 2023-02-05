4ab02be93b0887c95df583ae7b3e8f1cfb101f05
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-02-05 19:16:51 -0300

    Coloca fade nos níveis

--- GGJ_2023/Assets/Scripts/UI/MenuButton.cs
+++ GGJ_2023/Assets/Scripts/UI/MenuButton.cs
@@ -5,15 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject fade;
     public void GotoMainMenu()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene("MainMenu");
+        NextScene("MainMenu");
     }
     public void GotoLevelSelection()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene("LevelSelection");
+        NextScene("LevelSelection");
+    }
+
+    private IEnumerator ChangeScene(string scene)
+    {
+        fade.GetComponent<Animator>().Play("FadeOut");
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(scene);
+    }
+    public void NextScene(string scene)
+    {
+        StartCoroutine(ChangeScene(scene));
     }
 
 }
