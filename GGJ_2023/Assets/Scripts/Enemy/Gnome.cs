cfc4f1644c513af17521862254ba504f9be80af0
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-02-05 11:26:52 -0300

    Gnomo muda de sprite quando vida chega na metade

--- GGJ_2023/Assets/Scripts/Enemy/Gnome.cs
+++ GGJ_2023/Assets/Scripts/Enemy/Gnome.cs
@@ -15,7 +15,14 @@ public class Gnome : MonoBehaviour
     public void TakeDamage(float lifeDecrease)
     {
         if (life > lifeDecrease)
+        {
             life -= lifeDecrease;
+            if (life <= gnomeObject.endurance / 2)
+            {
+                //mudar animacao depois
+                GetComponent<SpriteRenderer>().sprite = gnomeObject.damagedSprite;
+            }
+        }
         else
             Destroy(this.gameObject);
     }
